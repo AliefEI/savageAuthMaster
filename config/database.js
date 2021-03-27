a4a78c536cbdcf8012194575be27e662d0589599
AliefEI <77537630+AliefEI@users.noreply.github.com>
2021-03-27 11:26:24 -0400

    Savage Auth Master
    Completed: 5
    Feel Good: 5

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+    //url is the same as the savage demo 
+    'url' : 'mongodb+srv://demo:demo@cluster0.iw35d.mongodb.net/demo?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
